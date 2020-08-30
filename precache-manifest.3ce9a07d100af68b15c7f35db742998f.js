d40f744317ae0b1f919e06313b8957338ab18571
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-08-30 12:52:48 -0500

    Updates

---
+++ precache-manifest.3ce9a07d100af68b15c7f35db742998f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17124c4694a54d0d26193e5d3a17801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8830907f8040bfa2093a",
+    "url": "/static/css/main.b4736dbd.chunk.css"
+  },
+  {
+    "revision": "7dfcd5818d616ec80ce9",
+    "url": "/static/js/2.08b316f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08b316f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8830907f8040bfa2093a",
+    "url": "/static/js/main.0a5048de.chunk.js"
+  },
+  {
+    "revision": "85356aa04eed2e6a0a3e",
+    "url": "/static/js/runtime-main.a39e060c.js"
+  },
+  {
+    "revision": "fd32d4f25bc11e8f8e83022e2f6f59ec",
+    "url": "/static/media/blue-metal-background.fd32d4f2.svg"
+  },
+  {
+    "revision": "a8b92cadd3f5f09eab33236c6b3175ed",
+    "url": "/static/media/dark-gray-metal-background.a8b92cad.svg"
+  },
+  {
+    "revision": "ed5ec1e3670c6d7f6a07e6bdaa86c93b",
+    "url": "/static/media/light-gray-metal-background.ed5ec1e3.svg"
+  },
+  {
+    "revision": "3979cb48ce83222368a37b2b4131f664",
+    "url": "/static/media/page-background.3979cb48.jpg"
+  },
+  {
+    "revision": "01242cada5836ecb80edd3d35eff09f0",
+    "url": "/static/media/profile-picture.01242cad.jpg"
+  }
+]);
