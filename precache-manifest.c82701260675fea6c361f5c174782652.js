d2f9e95f2d022a762c27233f52f80e198532d979
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-08-30 12:56:15 -0500

    Updates

---
+++ precache-manifest.c82701260675fea6c361f5c174782652.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b25907c493bd71d61474b6dc680db4",
+    "url": "/bootcamp-homework-020/index.html"
+  },
+  {
+    "revision": "5dc1c82bc68e1f420ae8",
+    "url": "/bootcamp-homework-020/static/css/main.2d0af7d6.chunk.css"
+  },
+  {
+    "revision": "7dfcd5818d616ec80ce9",
+    "url": "/bootcamp-homework-020/static/js/2.08b316f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bootcamp-homework-020/static/js/2.08b316f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc1c82bc68e1f420ae8",
+    "url": "/bootcamp-homework-020/static/js/main.70ee589b.chunk.js"
+  },
+  {
+    "revision": "4b931d1af6deedf1fe83",
+    "url": "/bootcamp-homework-020/static/js/runtime-main.43910785.js"
+  },
+  {
+    "revision": "fd32d4f25bc11e8f8e83022e2f6f59ec",
+    "url": "/bootcamp-homework-020/static/media/blue-metal-background.fd32d4f2.svg"
+  },
+  {
+    "revision": "a8b92cadd3f5f09eab33236c6b3175ed",
+    "url": "/bootcamp-homework-020/static/media/dark-gray-metal-background.a8b92cad.svg"
+  },
+  {
+    "revision": "ed5ec1e3670c6d7f6a07e6bdaa86c93b",
+    "url": "/bootcamp-homework-020/static/media/light-gray-metal-background.ed5ec1e3.svg"
+  },
+  {
+    "revision": "3979cb48ce83222368a37b2b4131f664",
+    "url": "/bootcamp-homework-020/static/media/page-background.3979cb48.jpg"
+  },
+  {
+    "revision": "01242cada5836ecb80edd3d35eff09f0",
+    "url": "/bootcamp-homework-020/static/media/profile-picture.01242cad.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ce9a07d100af68b15c7f35db742998f.js"
+  "/bootcamp-homework-020/precache-manifest.c82701260675fea6c361f5c174782652.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bootcamp-homework-020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
